b53e7e52a737c6bbc2667331bce46a918f8dd91f
BlackIP0 <arnaudbanier@yahoo.fr>
2017-03-13 16:08:01 +0100

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y openssl nginx shellinabox
+apt-get install -y openssl nginx shellinabox pwgen
+
+useradd pangu -d /opt/pangu -m -p P@ssW0rd
 
 exit 0
